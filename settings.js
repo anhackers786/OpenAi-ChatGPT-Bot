faa51714b7b73129c3c019c86fed68ae2d24c849
Awais Qadir <128619585+anhackers786@users.noreply.github.com>
2023-10-24 03:38:56 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,7 +28,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 > Ai Bot Password Get From Watching This Video 
 */
 global.AI_PASSWORD = 'king-nima-gpt-bot'
-global.SESSION_ID = 'elisa_md@;;;MZVRDJ4S#bCS9839QdDlC8isGudDt9CYhg69fVc8CvRenw7yf1Bw'
+global.SESSION_ID = 'elisa_md@;;;4E1U2TiS#3Arojws1f8wFvHpAs_qHTkLvIsW8zg9C7piq7clTNaE'
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
